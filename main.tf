1cff53f682144ee0737a78d1d2c81418e79b9a62
Álvaro G. Cachón <zaharus@gmail.com>
2021-05-10 08:41:29 +0200

    - Add prerequisites to the definition. - Decouple region from AWS data.

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.39"
+    }
+  }
+  
+  required_version = ">= 0.12"
+}
+
+variable "aws_region" {
+  description = "Region-for-AWS"
+  default = "no_region_value_found"
+}
+
 variable "aws_access_key" {
   description = "Access-key-for-AWS"
   default = "no_access_key_value_found"
@@ -6,6 +22,10 @@ variable "aws_access_key" {
 variable "aws_secret_key" {
   description = "Secret-key-for-AWS"
   default = "no_secret_key_value_found"
+}
+
+output "aws_region_is" {
+  value = var.aws_region
 }
 
 output "access_key_is" {
@@ -17,7 +37,7 @@ output "secret_key_is" {
 }
 
 provider "aws" {
-	region = "eu-west-3"
+	region = var.aws_region 
 	access_key = var.aws_access_key
 	secret_key = var.aws_secret_key
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
+aws_region = "REDACTED"
 aws_access_key = "REDACTED"
 aws_secret_key = "REDACTED"
