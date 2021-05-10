236dfd49f11fa40a348f759a625f66b496a7aaea
Álvaro G. Cachón <zaharus@gmail.com>
2021-05-10 08:30:13 +0200

    Decouple AWS keys from main.
      - Comment: the configuration file is not something that should be included in the repository, and should be generated by the environment when running the plan. I'm including in the commit for the sake of completion.

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
+variable "aws_access_key" {
+  description = "Access-key-for-AWS"
+  default = "no_access_key_value_found"
+}
+ 
+variable "aws_secret_key" {
+  description = "Secret-key-for-AWS"
+  default = "no_secret_key_value_found"
+}
+
 provider "aws" {
 	region = "eu-west-3"
-	access_key = "REDACTED"
-	secret_key = "REDACTED"
+	access_key = var.aws_access_key
+	secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "back-end" {
