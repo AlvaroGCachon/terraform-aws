18f9739e17d8dae06d15e80aaa0edcc831b32b0d
Álvaro G. Cachón <zaharus@gmail.com>
2021-05-12 08:43:44 +0200

    Configure gateway and routing.

--- main.tf
+++ main.tf
@@ -100,3 +100,20 @@ resource "aws_instance" "back-end" {
 		Name = "Bootstrap customer project"
 	}
 }
+
+resource "aws_internet_gateway" "back-end-gw" {
+  vpc_id = "${aws_vpc.back-end.id}"
+}
+
+resource "aws_route_table" "route-table-back-end" {
+  vpc_id = "${aws_vpc.back-end.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.back-end-gw.id}"
+  }
+}
+
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id      = "${aws_subnet.back-end.id}"
+  route_table_id = "${aws_route_table.route-table-back-end.id}"
+}
